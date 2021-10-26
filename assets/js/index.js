9c9e88c5d3a1a369c0a06c7230c9e8d6c5936d81
Yaremko Ivan <yaremich98@gmail.com>
2021-10-26 10:50:50 +0300

    Add structure slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,8 @@
+'use strict'
 
+const imagesDB = ['https://dekoriko.ru/images/article/croppedtop/290-435/2017/11/oformlenie-uzkoj-prihozhej.jpg',
+'https://st2.depositphotos.com/3873339/8013/i/600/depositphotos_80131050-stock-photo-realistic-square-picture-frame-on.jpg',
+'https://cdn.motor1.com/images/mgl/RWK9e/s3/2020-vw-arteon-r-line-edition.jpg'];
 
-function logMessage(){
-    console.log('hi!');
-}
-
-const [button] = document.getElementsByTagName('button');
-
-button.addEventListener('click', logMessage);
+const image = document.querySelector('.slide>img');
+const [prevBtn, nextBtn] = document.querySelectorAll('.slider-container>button');
