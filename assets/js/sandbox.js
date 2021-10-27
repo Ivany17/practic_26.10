78c33b5872fa9a33a10859e0da08d2a619e27e4f
Yaremko Ivan <yaremich98@gmail.com>
2021-10-27 10:02:45 +0300

    Add Adder

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -1,11 +1,15 @@
 'use strict';
 
-function createCounter(){
-    let i=0;
-    return function counter(){
-        return ++i;
-    }
-}
+// function createCounter(){
+//     let i=0;
+//     return function counter(){
+//         return ++i;
+//     }
+// }
 
-const counter1 = createCounter();
-const counter2 = createCounter();
+// const counter1 = createCounter();
+// const counter2 = createCounter();
+
+
+const createAdder = (n) => (m) => (n += m);
+const adder1 = createAdder(10);
